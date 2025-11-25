169db085b2abefea1be7473354df31c8878f60f7
Jose421 <josevromero2000@gmail.com>
2025-11-25 15:47:04 -0300

    Update tasksService.js

--- src/services/tasksService.js
+++ src/services/tasksService.js
@@ -1,5 +1,5 @@
 //tasksService.js
-const API_URL = "http://localhost:4000/api/tasks";
+const API_URL = `${import.meta.env.VITE_API_URL}/tasks`;
 
 // Obtener todas las tareas
 export async function getTasks() {
